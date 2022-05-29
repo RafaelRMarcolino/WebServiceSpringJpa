52388886817ef167b8bca760a01891329cc19480
rafa <ralfrr55@gmail.com>
2022-05-29 08:08:29 -0300

    update users

--- src/main/java/com/example/demo/services/UserServices.java
+++ src/main/java/com/example/demo/services/UserServices.java
@@ -3,11 +3,16 @@ package com.example.demo.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Users;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.services.exception.DataBaseException;
 import com.example.demo.services.exception.ResourceNotFoundException;
 
 @Service
@@ -15,39 +20,73 @@ public class UserServices {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<Users> find(){
-			
+
+	public List<Users> find() {
+
 		return repo.findAll();
 	}
-	
+
 	public Users findById(Long id) {
 		Optional<Users> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
-		
+
 	}
-	
+
 	public Users insert(Users obj) {
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		repo.deleteById(id);
-	}
-	
-	public Users update(Long id, Users obj) {
+		try {
+			repo.deleteById(id);
+
+		}catch (EmptyResultDataAccessException e) {
+			
+			throw new ResourceNotFoundException(id);
+		}/*
+		saber qual sera a excecao
 		
-		Users entity = repo.getById(id);
-		updateData(entity, obj);
-		return repo.save(entity);
+		catch (RuntimeException e) {
+
+			e.printStackTrace();
+		
+		}*/
+		
+		catch(DataIntegrityViolationException e) {
+			
+			throw new DataBaseException(e.getMessage());
+			
+		}
+		
+	}
+
+	public Users update(Long id, Users obj) {
+		try {
+
+			Users entity = repo.getById(id);
+			updateData(entity, obj);
+			return repo.save(entity);
+
+		}/* achar a excecao
+		
+		catch (RuntimeException e) {
+
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+
+		}*/
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+
+		}
 	}
 
 	private void updateData(Users entity, Users obj) {
-		
+
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
-	
+
 }
